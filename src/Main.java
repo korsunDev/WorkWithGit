a5c4336906e111b5c973f755c7561b7a770fccb0
Andrey Korsun <korsun.email@gmail.com>
2016-06-13 00:50:26 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-/**
- * Created by Justsmile on 13.06.2016.
- */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Second line");
     }
 }
